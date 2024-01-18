3b603a2a4612fd0de098827220dc28c42e338814
Alex <alex.likes.robots@gmail.com>
2024-01-18 13:10:18 -0500

    Finished :)

--- CIW.lesson6.labs/personalLab.js
+++ CIW.lesson6.labs/personalLab.js
@@ -1,57 +1,50 @@
- function addNames (arg1, arg2) {
-        var result = arg1 + " "+ arg2;
-        return result;
-      }
-      
-var nam1 = prompt("Please enter your first name.","");
-var nam2 = prompt("Please enter your last name","");
-    
-document.write("Hello " + addNames((nam1), (nam2)) + " your quiz is ready for you");
+var nam1 = prompt("Please enter your first name.", "");
+var nam2 = prompt("Please enter your last name", "");
 
-
-function vote()
-{
-   var t=""
-   for (i=0; i<document.myform.sports.length; i++)
-   {
-      if (document.myform.sports[i].checked==true)
-      {
-         t =t +document.myform.sports[i].value
-      }
-   }
+function addNames(arg1, arg2) {
+    return arg1 + " " + arg2;
 }
-let numCorrect = 0;
+document.write("Hello " + addNames((nam1), (nam2)) + " your quiz is ready for you");
+function getSelectedSports() {
+    var selectedSports = "";
+    for (i = 0; i < document.sport.sports.length; i++) {
+        if (document.sport.sports[i].checked == true) {
+            selectedSports += document.sport.sports[i].value;
+        }
+    }
+    return selectedSports;
+}
 
 function takeTest() {
-    let response="";
-	let points = 0;
+    var response = "";
+    var points = 0;
 
-	let q1 = "What is "+ nam1 +"'s favorite color?";
-	let a1 = "Nope";
+    var q1 = "What is " + nam1 + "'s favorite color?";
+    var a1 = "IDK";
+    
+    var len = nam2.length
+    var q2 = "How many letters are in " + nam2;
+    var a2 = len;
 
-	let q2 = "How many letters are in "+ nam2;
-	let a2 = 3;
+    var selectedSports = getSelectedSports();
+    var q3 = "What is " + addNames(nam1, nam2) + "'s favorite sport?";
+    var a3 = selectedSports;
 
-	let q3 = "What is "+ addNames(nam1, nam2) +" favorite sport?";
-	let a3 = t;
+    response = prompt(q1, "");
+    if (response) points = runningTotal((response.toUpperCase() == a1) ? 1 : 0);
 
-	response = prompt(q1,"");
-	if (response) points = runningTotal((response.toUpperCase() == a1) ? 1 : 0);
-	alert(points);
+    response = prompt(q2, "");
+    if (response) points = runningTotal((response == a2) ? 1 : 0);
 
-	response = prompt(q2,"");
-	if (response) points = runningTotal((response == a2) ? 1 : 0);
-	alert(points);
+    response = prompt(q3, "");
+    if (response) points = runningTotal((response.toUpperCase() == a3) ? 1 : 0);
 
-	response = prompt(q3,"");
-	if (response) points = runningTotal((response.toUpperCase() == a3) ? 1 : 0);
-	alert("You answered a total of " + points + " correctly.");
-
-	numCorrect = 0;
-	points = 0;
+    alert("Hello " + addNames(nam1, nam2) + "! You answered a total of " + points + " correctly.");
 }
 
+var numCorrect = 0;
+
 function runningTotal(i) {
-  numCorrect += i; 
-  return numCorrect;
+    numCorrect += i;
+    return numCorrect;
 }
